9dbdf9122fc82d9091870fea02109f0befc6304c
SamandarTemirxodjayev <iwtuz166@gmail.com>
2023-04-26 14:54:30 +0500

    add skeleton and fix some bugs

--- components/Menu.vue
+++ components/Menu.vue
@@ -1,5 +1,59 @@
 <template>
-  <div>
+  <div v-if="loading" class="">
+    <div class="" v-for="item in 4" :key="item">
+      <div role="status" class="animate-pulse">
+        <div
+          class="h-5 bg-gray-200 rounded-full dark:bg-gray-400 w-[80%] m-2"
+        ></div>
+        <span class="sr-only">Loading...</span>
+      </div>
+      <div class="overflow-x-auto flex">
+        <div
+          role="status"
+          class=" p-2 rounded animate-pulse md:p-4 w-full"
+        >
+          <div
+            class="items-center justify-center h-48 bg-gray-300 rounded dark:bg-gray-400"
+          >
+            <svg
+              class="w-12 h-12 text-gray-200 dark:text-gray-400"
+              xmlns="http://www.w3.org/2000/svg"
+              aria-hidden="true"
+              fill="currentColor"
+              viewBox="0 0 640 512"
+            >
+              <path
+                d="M480 80C480 35.82 515.8 0 560 0C604.2 0 640 35.82 640 80C640 124.2 604.2 160 560 160C515.8 160 480 124.2 480 80zM0 456.1C0 445.6 2.964 435.3 8.551 426.4L225.3 81.01C231.9 70.42 243.5 64 256 64C268.5 64 280.1 70.42 286.8 81.01L412.7 281.7L460.9 202.7C464.1 196.1 472.2 192 480 192C487.8 192 495 196.1 499.1 202.7L631.1 419.1C636.9 428.6 640 439.7 640 450.9C640 484.6 612.6 512 578.9 512H55.91C25.03 512 .0006 486.1 .0006 456.1L0 456.1z"
+              />
+            </svg>
+          </div>
+          <span class="sr-only">Loading...</span>
+        </div>
+        <div
+          role="status"
+          class="max-w-sm p-2 rounded animate-pulse md:p-4 w-full"
+        >
+          <div
+            class="flex items-center justify-center h-48 bg-gray-300 rounded dark:bg-gray-400"
+          >
+            <svg
+              class="w-12 h-12 text-gray-200 dark:text-gray-400"
+              xmlns="http://www.w3.org/2000/svg"
+              aria-hidden="true"
+              fill="currentColor"
+              viewBox="0 0 640 512"
+            >
+              <path
+                d="M480 80C480 35.82 515.8 0 560 0C604.2 0 640 35.82 640 80C640 124.2 604.2 160 560 160C515.8 160 480 124.2 480 80zM0 456.1C0 445.6 2.964 435.3 8.551 426.4L225.3 81.01C231.9 70.42 243.5 64 256 64C268.5 64 280.1 70.42 286.8 81.01L412.7 281.7L460.9 202.7C464.1 196.1 472.2 192 480 192C487.8 192 495 196.1 499.1 202.7L631.1 419.1C636.9 428.6 640 439.7 640 450.9C640 484.6 612.6 512 578.9 512H55.91C25.03 512 .0006 486.1 .0006 456.1L0 456.1z"
+              />
+            </svg>
+          </div>
+          <span class="sr-only">Loading...</span>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div v-else>
     <div v-for="catalog in catalogs" :key="catalog._id" class="cursor-pointer">
       <h2 class="text-2xl font-bold p-1">{{ catalog.name }}</h2>
       <div class="container">
@@ -10,8 +64,8 @@
             class="rounded-t-lg mx-2 my-2 flex-none"
             style="width: 300px"
           >
-            <nuxt-link :to="`/menu/${menu._id}`">
-              <menu-img :url="menu.photo_url"/>
+            <NuxtLink :to="`/menu/${menu._id}`">
+              <menu-img :url="menu.photo_url" />
               <div class="py-1">
                 <h3 class="font-bold text-2xl mb-2">{{ menu.name }}</h3>
                 <div class="flex justify-between text-end">
@@ -20,7 +74,7 @@
                   </span>
                 </div>
               </div>
-            </nuxt-link>
+            </NuxtLink>
           </div>
         </div>
       </div>
@@ -29,38 +83,33 @@
 </template>
 
 <script>
-import axios from 'axios'
-import MenuImg from './MenuImg.vue'
+import axios from "axios";
+import MenuImg from "./MenuImg.vue";
 
 export default {
   components: { MenuImg },
-  props:{
+  props: {
     catalogs: {
       type: Array,
-      required: true
+      required: true,
     },
   },
   data() {
     return {
       loading: true,
-      menus: []
-    }
+      menus: [],
+    };
   },
-  created() {
-    axios.get('https://restoranmenu1.vercel.app/menu').then((res) => {
-      this.menus = res.data.menus
-    })
+  async created() {
+    await axios.get("https://restoranmenu1.vercel.app/menu").then((res) => {
+      this.menus = res.data.menus;
+      this.loading = false;
+    });
   },
   methods: {
     filteredMenus(categoryId) {
-      return this.menus.filter((menu) => menu.category_id === categoryId)
-    },
-    handleOnload() {
-      const timer = setTimeout(() => {
-        this.loading = false
-      }, 2000)
-      return () => clearTimeout(timer)
+      return this.menus.filter((menu) => menu.category_id === categoryId);
     },
   },
-}
+};
 </script>
